12238944b1e6af656c1f94ecc225c0c6052d6dce
Areejshaikh <160318564+Areejshaikh@users.noreply.github.com>
2024-04-12 00:41:22 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,15 @@
+import inquirer from "inquirer";
+const bmi = await inquirer.prompt([
+    {
+        name: "weight",
+        type: "string",
+        message: "Enter your weight in kg?"
+    },
+    {
+        name: "height",
+        type: "string",
+        message: "Enter your height in meters?"
+    }
+]);
+let BMI = bmi.weight / (bmi.height * bmi.height);
+console.log(`your bmi is ${BMI}`);
